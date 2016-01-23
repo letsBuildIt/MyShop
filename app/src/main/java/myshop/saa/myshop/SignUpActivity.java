9e412b2742ceb3824a0c0b009528247cf06ccf3f
AnkurBijalwan <bijalwan.ankur@gmail.com>
2016-01-23 20:20:14 +0530

    Updating Code

--- app/src/main/java/myshop/saa/myshop/SignUpActivity.java
+++ app/src/main/java/myshop/saa/myshop/SignUpActivity.java
@@ -1,33 +1,25 @@
 package myshop.saa.myshop;
 
-import android.app.Activity;
-import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.view.View;
 
-public class MainActivity extends Activity {
 
-    Button signUp;
+public class SignUpActivity extends ActionBarActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.signup);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_signup, menu);
         return true;
-    }
-
-    public void navigateSignUp(View view){
-        Intent intent=new Intent(this,SignUpActivity.class);
-        startActivity(intent);
     }
 
     @Override
